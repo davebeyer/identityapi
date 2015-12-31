83af4c4bd03a27181041623c93bae0b1412412dd
Dave Beyer <dave.beyer@gmail.com>
2015-12-30 22:22:16 -0800

    Switch to typescript

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-exports.testMsg = function() {
+/// <reference path="./typings/tsd.d.ts" />
+
+export function testMsg() {
   console.log("Test message from the Lighthouse User Identity API module");
 }
